b1296936b2eb08a68a037b9148f1f9d563cae82b
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-28 00:33:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
-document.addEventListener("DOMContentloaded", (e) => {
+// document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
-});
+
+
+// 1. Find out which button was pressed
+// 2. Get the data for that movie
+// 3. Replace the template values with the real ones
+
+const titanicButton = document.getElementById('Titanic')
+const terminatorButton = document.getElementById('Terminator 2')
+
+function onMovieChange (movie) {
+  let data = movies[movie]
+    // Replace template values
+  let title = data.title // "Titanic"
+  let titleDiv = document.getElementById('title')
+  titleDiv.innerHTML = title
+  
+  let directorDiv = document.getElementById('director')
+  directorDiv.innerHTML = data.director
+  
+   let genreDiv = document.getElementById('genre')
+  genreDiv.innerHTML = data.genre
+  
+   let filmRatingDiv = document.getElementById('filmRating')
+  filmRatingDiv.innerHTML = data.filmRating
+  
+  let posterImg = document.getElementById('poster')
+  posterImg.src = data.poster
+  
+   let audienceScoreDiv = document.getElementById('audienceScore')
+  audienceScoreDiv.innerHTML = data.audienceScore
+  
+  let descriptionDiv = document.getElementById('description')
+  descriptionDiv.innerHTML = data.description
+  
+  let castDiv = document.getElementById('cast')
+  
+  let startingString = ''
+  for (let castMember of data.cast) {
+   startingString += '<li>' + castMember.role + ': ' + 
+      castMember.actor + '</li>'
+      
+  castDiv.innerHTML = startingString
+  
+  //
+  
+  const reviewsDiv = document.getElementById('reviews')
+  
+  startingString = ''
+  for (let reviews of data.reviews) {
+   startingString += '<li>' + reviews.username + ': ' + 
+      reviews.content + '</li>'
+      
+  reviewsDiv.innerHTML = startingString
+  
+  // <li>Role: Name of actor</li>
+}
+}
+}
+
+
+titanicButton.addEventListener('click', () => onMovieChange('Titanic'))
+
+terminatorButton.addEventListener('click', () => onMovieChange('Terminator 2'))
+
+onMovieChange('Titanic')
+
+// });
